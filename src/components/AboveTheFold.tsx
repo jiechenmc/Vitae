cb7171605d650cd2e6dc1ca08ff0333e90a42e27
Jie Chen <jiechenmc@gmail.com>
2022-05-20 22:00:01 -0400

    improved code reability by putting the svg icons in their own file and moving view resume button to its own component

--- src/components/AboveTheFold.tsx
+++ src/components/AboveTheFold.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Avatar from "./Avatar";
 import SocialMediaBar from "./SocialMediaBar";
+import ViewResumeButton from "./ViewResumeButton";
+import { ReactComponent as DownArrow } from "../assets/DownArrow.svg";
 
 const AboveTheFold = () => {
   return (
@@ -18,28 +20,9 @@ const AboveTheFold = () => {
             <h4 className="font-semibold leading-none mb-6">
               <SocialMediaBar />
             </h4>
-            <a
-              className="inline-block px-7 py-3 mb-1 border-2 border-gray-500 text-white font-medium text-sm leading-snug uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
-              href="https://drive.google.com/file/d/1YYvgQczc_2N1tn6oSFrAqZYatgVpykkL/view?usp=sharing"
-              target="_blank"
-              rel="noreferrer"
-              role="button"
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-            >
-              View Resume
-            </a>
+            <ViewResumeButton />
             <div className="appearance-none flex justify-center items-end mt-5">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-                className="animate-bounce w-6 h-6"
-              >
-                <path
-                  fill="#FFFFFF"
-                  d="M256 0C114.6 0 0 114.6 0 256c0 141.4 114.6 256 256 256s256-114.6 256-256C512 114.6 397.4 0 256 0zM382.6 302.6l-103.1 103.1C270.7 414.6 260.9 416 256 416c-4.881 0-14.65-1.391-22.65-9.398L129.4 302.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L224 306.8V128c0-17.69 14.33-32 32-32s32 14.31 32 32v178.8l49.38-49.38c12.5-12.5 32.75-12.5 45.25 0S395.1 290.1 382.6 302.6z"
-                />
-              </svg>
+              <DownArrow />
             </div>
           </div>
         </div>
---
+++ src/components/ViewResumeButton.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ViewResume = () => {
+  return (
+    <a
+      className="inline-block px-7 py-3 mb-1 border-2 border-gray-500 text-white font-medium text-sm leading-snug uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+      href="https://drive.google.com/file/d/1YYvgQczc_2N1tn6oSFrAqZYatgVpykkL/view?usp=sharing"
+      target="_blank"
+      rel="noreferrer"
+      role="button"
+      data-mdb-ripple="true"
+      data-mdb-ripple-color="light"
+    >
+      View Resume
+    </a>
+  );
+};
+
+export default ViewResume;
