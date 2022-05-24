0ece91882213e64d842bd648372c482eebc493e9
Jie Chen <jiechenmc@gmail.com>
2022-05-24 14:50:22 -0400

    ported CRA project to Vite

--- README.md
+++
@@ -1,1 +0,0 @@
-My portfolio site that will host my resume and my projects.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/tw-elements/dist/js/**/*.js",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
