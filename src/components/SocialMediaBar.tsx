948a449bcf4c82363e2291a678c2b99b751dbb20
Jie Chen <jiechenmc@gmail.com>
2022-06-13 23:31:45 -0500

    removed unnesscary animations

--- src/components/SocialMediaBar.tsx
+++ src/components/SocialMediaBar.tsx
@@ -9,8 +9,6 @@ const SocialMediaBar = () => {
       <a href="https://github.com/jiechenmc" target="_blank" rel="noreferrer">
         <button
           type="button"
-          data-mdb-ripple="true"
-          data-mdb-ripple-color="light"
           className="inline-block px-6 py-2.5 mb-2 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out bg-[#333]"
         >
           <GithubLogo className="w-4 h-4" />
@@ -24,8 +22,6 @@ const SocialMediaBar = () => {
       >
         <button
           type="button"
-          data-mdb-ripple="true"
-          data-mdb-ripple-color="light"
           className="inline-block px-6 py-2.5 mb-2 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out bg-[#0077b5]"
         >
           <LinkedinLogo className="w-4 h-4" />
