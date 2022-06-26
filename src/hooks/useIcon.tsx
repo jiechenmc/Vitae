62dec5275ed962632bb2f89f6a28d62390d2e78f
Jie Chen <jiechenmc@gmail.com>
2022-06-25 22:21:15 -0500

    modulized icons

--- src/hooks/useIcon.tsx
+++ src/hooks/useIcon.tsx
@@ -1,23 +1,9 @@
-import { Icon } from "@iconify/react";
-
 import React, { useState, useEffect } from "react";
+import table from "../data/ICONS";
+import { Icon } from "@iconify/react";
 
 const useIcon = (icon: string) => {
   const [_icon, _setIcon] = useState<React.ReactElement>();
-  const table: { [key: string]: React.ReactElement } = {
-    react: <Icon icon="logos:react" />,
-    playwright: <Icon icon="logos:playwright" />,
-    typescript: <Icon icon="logos:typescript-icon" />,
-    python: <Icon icon="logos:python" />,
-    firebase: <Icon icon="logos:firebase" />,
-    mysql: <Icon icon="logos:mysql-icon" />,
-    flask: <Icon icon="file-icons:flask" />,
-    docker: <Icon icon="logos:docker-icon" />,
-    bash: <Icon icon="logos:bash-icon" />,
-    fastapi: <Icon icon="simple-icons:fastapi" color="#089484" />,
-    mongodb: <Icon icon="vscode-icons:file-type-mongo" />,
-    digitalocean: <Icon icon="simple-icons:digitalocean" color="#0069ff" />,
-  };
 
   useEffect(() => {
     _setIcon(table[icon] ?? <Icon icon="emojione:warning" />);
