f59f90dc684af2424f4a89a05ee502fbba9d94b6
Jie Chen <jiechenmc@gmail.com>
2022-06-30 00:07:28 -0500

    added most used languages

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -17,6 +17,9 @@ const Projects = () => {
       <h1 className="relative text-2xl light:text-gray-700 dark:text-base-content font-bold text-center mt-3 mb-3">
         Featured Projects
       </h1>
+      <div className="flex justify-center mb-6">
+        <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=jiechenmc&layout=compact&theme=dark&hide_border=true" />
+      </div>
       <div className="sm:flex-col md:grid md:grid-cols-3 justify-between">
         {[...PROJECTS]?.reverse().map((project: ProjectElement) => (
           <ProjectEntries
