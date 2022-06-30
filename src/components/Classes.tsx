162a97eb3ed7c4ac2a940c077a71f2f9e0ec6dcc
Jie Chen <jiechenmc@gmail.com>
2022-06-29 23:25:48 -0500

    updated education section

--- src/components/Classes.tsx
+++ src/components/Classes.tsx
@@ -1,6 +1,7 @@
 import ClassEntries from "./subcomponents/ClassEntries";
 import JOURNIES from "../data/CLASSES";
 import { v4 as uuidv4 } from "uuid";
+import SBULogo from "../assets/sbu.webp";
 
 interface JourneyElement {
   date: string;
@@ -14,6 +15,18 @@ const Classes = () => {
       <h1 className="relative text-2xl text-base-content font-bold text-center my-3">
         Education
       </h1>
+
+      <div className="flex justify-center mb-6 gap-2">
+        <img className="" src={SBULogo} width="96px" />
+        <div className="whitespace-nowrap ">
+          <p className="font-bold w-36 ">Stony Brook University</p>
+          <p className="font-bold w-36 ">B.S. — Computer Science</p>
+          <small className="font-bold ">Expected Graduation: May 2025</small>
+          <br />
+          <small className="font-bold">GPA: 3.49</small>
+        </div>
+      </div>
+
       <div className="flex justify-center ml-3">
         <ol className="border-l border-gray-300">
           {[...JOURNIES]?.reverse().map((journey: JourneyElement) => (
--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -14,12 +14,9 @@ const Head = () => {
                 <img src={worry} />
               </div>
             </div>
-            <h1>
-              <img src="https://readme-typing-svg.herokuapp.com?duration=3500&color=000000&center=true&vCenter=true&lines=Hi+I+am+Jie+Chen!;My+interests+are+...;Data+Engineering+%F0%9F%97%84%EF%B8%8F!;Production+Engineering+%F0%9F%92%BF!;and+Software+Engineering+%F0%9F%91%A8%E2%80%8D%F0%9F%92%BB!+" />
-            </h1>
+            <img src="https://readme-typing-svg.herokuapp.com?duration=3500&color=67738A&center=true&vCenter=true&lines=Hi+I+am+Jie+Chen!;My+interests+are+...;Data+Engineering+%F0%9F%97%84%EF%B8%8F!;Production+Engineering+%F0%9F%92%BF!;and+Software+Engineering+%F0%9F%91%A8%E2%80%8D%F0%9F%92%BB!+" />
             <p className="pb-6">
-              I am currently a sophomore at Stony Brook University studying
-              Computer Science!
+              Stony Brook University '25 — B.S. Computer Science
             </p>
             <ViewResumeButton />
           </div>
