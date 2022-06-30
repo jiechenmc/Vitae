ece093d1336e0a7838ff15d24f3da617ce9bd3b9
Jie Chen <jiechenmc@gmail.com>
2022-06-30 01:32:55 -0500

    removed unused css

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -14,7 +14,7 @@ interface ProjectElement {
 const Projects = () => {
   return (
     <div id="projects">
-      <h1 className="relative text-2xl light:text-gray-700 dark:text-base-content font-bold text-center mt-3 mb-3">
+      <h1 className=" text-2xl light:text-gray-700 dark:text-base-content font-bold text-center mt-3 mb-3">
         Featured Projects
       </h1>
       <div className="flex justify-center mb-6">
