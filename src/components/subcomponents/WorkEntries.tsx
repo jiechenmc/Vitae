e83225a3325f50fdc1883e7ab7626b886f3be27e
Jie Chen <jiechenmc@gmail.com>
2022-07-10 02:34:51 -0500

    try max fit

--- src/components/subcomponents/WorkEntries.tsx
+++ src/components/subcomponents/WorkEntries.tsx
@@ -1,14 +1,15 @@
-import WorkDescription from "./WorkDescription";
 import { WorkElement } from "../Work";
 
 const WorkEntries = ({ period, company, role, description }: WorkElement) => {
-  const desc = description
-    .split("\n")
-    .map((e) => <WorkDescription description={e} />);
+  const desc = description.split("\n").map((e) => (
+    <pre data-prefix=">">
+      <code>{e}</code>
+    </pre>
+  ));
 
   return (
     <div className="flex justify-center">
-      <div className="mockup-code max-w-sm lg:max-w-fit w-full">
+      <div className="mockup-code max-w-fit w-full">
         <pre data-prefix="!" className="text-info">
           <code>{period}</code>
         </pre>
