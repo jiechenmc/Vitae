8c084986019bd4b6334953331be2521c20d95780
Jie Chen <jiechenmc@gmail.com>
2022-11-07 20:23:44 -0500

    added button for outdated projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -13,27 +13,55 @@ export interface ProjectElement {
 }
 
 const Projects = () => {
+  const recentProjects = PROJECTS.filter((e) => e.status != "").reverse();
+  const outdatedProjects = PROJECTS.filter((e) => e.status == "");
   return (
     <div id="projects" className="">
-      <h1 className="my-4 text-2xl light:text-gray-700 dark:text-base-content font-bold text-center">
-        Projects
-      </h1>
+      <div className="drawer">
+        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
+          <h1 className="my-4 text-2xl light:text-gray-700 dark:text-base-content font-bold text-center">
+            Projects
+          </h1>
+          <div className="text-center my-2">
+            <label htmlFor="my-drawer" className="btn btn-accent drawer-button">
+              Show Outdated Projects
+            </label>
+          </div>
 
-      <div className="sm:flex-col lg:grid lg:grid-cols-3 hd:grid-cols-4 mba:grid-cols-5 justify-between ">
-        {[...PROJECTS]?.reverse().map((project: ProjectElement) => (
-          <ProjectEntries
-            key={uuidv4()}
-            title={project?.title}
-            date={project?.date}
-            description={project?.description}
-            repo={project?.repo}
-            status={project?.status}
-            stack={project?.stack}
-            demo={project?.demo}
-          />
-        ))}
+          <div className="sm:flex-col lg:grid lg:grid-cols-3 hd:grid-cols-4 mba:grid-cols-5 justify-between ">
+            {recentProjects.map((project: ProjectElement) => (
+              <ProjectEntries
+                key={uuidv4()}
+                title={project?.title}
+                date={project?.date}
+                description={project?.description}
+                repo={project?.repo}
+                status={project?.status}
+                stack={project?.stack}
+                demo={project?.demo}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+            {outdatedProjects.map((project: ProjectElement) => (
+              <ProjectEntries
+                key={uuidv4()}
+                title={project?.title}
+                date={project?.date}
+                description={project?.description}
+                repo={project?.repo}
+                status={project?.status}
+                stack={project?.stack}
+                demo={project?.demo}
+              />
+            ))}
+          </ul>
+        </div>
       </div>
-
       <div className="flex justify-center">
         <img
           className="w-full md:w-[60%] lg:w-[45%] xl:w-[35%] 2xl:w-[25%]"
