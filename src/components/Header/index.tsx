fa8181266b59259685cb5ffeeca39271b17488ed
Jie Chen <jiechenmc@gmail.com>
2023-06-23 23:52:00 -0400

    put svg into public

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,7 +11,7 @@ const Head = () => {
                 <div className="hero-content grid lg:flex-row justify-center p-0">
 
                     <img src={defaultThumbnail} className="max-w-sm rounded-lg shadow-2xl ml-auto mr-auto" />
-                    <img id="AnimatedIntroduction" className="object-center text-center justify-center flex" src={"src/components/Header/darkTypeWriter.svg"} />
+                    <img id="AnimatedIntroduction" className="object-center text-center justify-center flex" src={"/darkTypeWriter.svg"} />
 
                     <div className="hero-content flex-col">
                         <div className="flex gap-2">
